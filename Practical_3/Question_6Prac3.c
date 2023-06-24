f84e2a3b93110bc5377b8bfa3b5f7bf517c324a0
S. D. Nipunanga <43111462+Nibbles0@users.noreply.github.com>
2023-06-24 12:35:25 +0530

    Question_6Prac3.c

--- Practical_3/Question_6Prac3.c
+++ Practical_3/Question_6Prac3.c
@@ -2,15 +2,20 @@
 
 int main()
 {
-    int up = 'A B C';
-    int low = "a b c";
-    int num = "0 1 2";
-    int weird = "$ * + /";
-    int weirder = " ";
-
-    printf("%d\n %d\n %d\n %d\n %d", up, low, num, weird, weirder);
-
-
+    printf("ASCII value of A: %d\n", 'A');
+    printf("ASCII value of B: %d\n", 'B');
+    printf("ASCII value of C: %d\n", 'C');
+    printf("ASCII value of a: %d\n", 'a');
+    printf("ASCII value of b: %d\n", 'b');
+    printf("ASCII value of c: %d\n", 'c');
+    printf("ASCII value of 0: %d\n", '0');
+    printf("ASCII value of 1: %d\n", '1');
+    printf("ASCII value of 2: %d\n", '2');
+    printf("ASCII value of $: %d\n", '$');
+    printf("ASCII value of *: %d\n", '*');
+    printf("ASCII value of +: %d\n", '+');
+    printf("ASCII value of /: %d\n", '/');
+    printf("ASCII value of blank character: %d\n", ' ');
 
     return 0;
 }
