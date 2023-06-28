65f60c666402012c00181353a78b67f2bb6c1ca9
S. D. Nipunanga <43111462+Nibbles0@users.noreply.github.com>
2023-06-28 16:38:09 +0530

    Update Question_11Prac5.c

--- Practical_5/Question_11Prac5.c
+++ Practical_5/Question_11Prac5.c
@@ -1,53 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     //Write a C program to print all factors of a given integer.
 
-    int num, count = 1, i = 0;
+    int num, count = 1;
 
     printf("Enter a numerical input: ");
     scanf("%d", &num);
+
+    printf("The factors of %d, is: ", num);
 
     while (count <= num)
     {
         if ((num%count) == 0)
         {
-            ++i;
+            printf("%d ", count);
             ++count;
         }
         else if((num%count) > 0)
         {
             ++count;
         }
-    }
-
-    int ii[i];
-    int count1 = 0;
-    count = 1;
-
-    int value = i;
-
-    while (count1 <= value)
-    {
-        if ((num%count) == 0)
-        {
-            ii[count1] = num;
-            ++count1;
-            ++count;
-        }
-        else if((num%count) > 0)
-        {
-            ++count;
-        }
-    }
-
-    for (count = 0; count <= i; ++count)
-    {
-        printf("%d, ", ii[count]);
     }
 
 
     return 0;
 }
+
