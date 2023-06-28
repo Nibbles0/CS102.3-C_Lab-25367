205e9ad04a8454aa2cae01a1832e6c880c91c7dc
S. D. Nipunanga <43111462+Nibbles0@users.noreply.github.com>
2023-06-28 15:53:24 +0530

    Create Question_11Prac5.c
    An error was given when running the code, will have to check further into it in order to fix whatever mistake had happened.

---
+++ Practical_5/Question_11Prac5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Write a C program to print all factors of a given integer.
+
+    int num, count = 1, i = 0;
+
+    printf("Enter a numerical input: ");
+    scanf("%d", &num);
+
+    while (count <= num)
+    {
+        if ((num%count) == 0)
+        {
+            ++i;
+            ++count;
+        }
+        else if((num%count) > 0)
+        {
+            ++count;
+        }
+    }
+
+    int ii[i];
+    int count1 = 0;
+    count = 1;
+
+    int value = i;
+
+    while (count1 <= num)
+    {
+        if ((num%count) == 0)
+        {
+            ii[count1] = num;
+            ++count1;
+            ++count;
+        }
+        else if((num%count) > 0)
+        {
+            ++count;
+        }
+        for (count = 0; count <= i; ++count)
+        {
+            printf("%d, ", ii[count]);
+        }
+    }
+
+
+    return 0;
+}
